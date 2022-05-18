4e69f51fe9cc2964dfb3f73315a59f55e1317f8b
Giuliana <giuliana_castellini@hotmail.com>
2022-05-17 23:06:26 -0300

    initial

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('./dist/proyecto'));
+app.get('/*',(req,res)=>
+res.sendFile('index.html',{root:'dist/proyecto'}),
+);
+app.listen(process.env.PORT || 8080);
